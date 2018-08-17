7718f0112337b112d0c373b875d3f37163c3aae7
kujyp <pjo901018@gmail.com>
2018-08-17 13:03:07 +0900

    Open synchronized socket

--- src/main/java/com/yangyinetwork/App.java
+++ src/main/java/com/yangyinetwork/App.java
@@ -1,13 +1,42 @@
 package com.yangyinetwork;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        openSocket();
+    }
+
+    private static void openSocket() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+
+            Socket accepted = serverSocket.accept();
+            acceptInputStream(accepted.getInputStream());
+            accepted.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void acceptInputStream(InputStream inputStream) {
+//        while (true) {
+        try {
+            byte[] buff = new byte[100];
+
+            int read = inputStream.read(buff);
+            System.out.printf("read: %d %n", read);
+            System.out.println(new String(buff));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        }
     }
 }
